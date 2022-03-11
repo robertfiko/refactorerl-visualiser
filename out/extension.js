2e46c98d550056fe3147a19d87083461d6300dd0
Robert Fiko <64466886+robertfiko@users.noreply.github.com>
2022-03-11 22:04:24 +0100

    Inital commit

---
+++ out/extension.js
@@ -0,0 +1,194 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+const vscode = require("vscode");
+function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand('catCoding.start', () => {
+        CatCodingPanel.createOrShow(context.extensionUri);
+    }));
+    if (vscode.window.registerWebviewPanelSerializer) {
+        // Make sure we register a serializer in activation event
+        vscode.window.registerWebviewPanelSerializer(CatCodingPanel.viewType, {
+            async deserializeWebviewPanel(webviewPanel, state) {
+                console.log(`Got state: ${state}`);
+                // Reset the webview options so we use latest uri for `localResourceRoots`.
+                webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+                CatCodingPanel.revive(webviewPanel, context.extensionUri);
+            }
+        });
+    }
+    context.subscriptions.push(vscode.commands.registerCommand('catCoding.doRefactor', () => {
+        if (CatCodingPanel.currentPanel) {
+            CatCodingPanel.currentPanel.doRefactor();
+        }
+    }));
+}
+exports.activate = activate;
+function getWebviewOptions(extensionUri) {
+    return {
+        // Enable javascript in the webview
+        enableScripts: true,
+        // And restrict the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+    };
+}
+/**
+ * Manages cat coding webview panels
+ */
+class CatCodingPanel {
+    constructor(panel, extensionUri) {
+        this._disposables = [];
+        this.referlOutputDiscovered = false;
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+        // Set the webview's initial html content
+        this._update();
+        // Listen for when the panel is disposed
+        // This happens when the user closes the panel or when the panel is closed programmatically
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+        // Update the content based on view changes
+        this._panel.onDidChangeViewState(e => {
+            if (this._panel.visible) {
+                //TODO: JS script should get some kind of inital thing as this is a thign
+                //this._update();
+            }
+        }, null, this._disposables);
+        // Handle messages from the webview
+        this._panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'alert':
+                    vscode.window.showErrorMessage(message.text);
+                    return;
+            }
+        }, null, this._disposables);
+        if (vscode.workspace.workspaceFolders !== undefined) {
+            const workspaceUri = vscode.workspace.workspaceFolders[0].uri;
+            const outputFileName = "/.referloutput.json";
+            const outputFilePath = vscode.Uri.joinPath(workspaceUri, outputFileName);
+            const watcher = vscode.workspace.createFileSystemWatcher(outputFilePath.fsPath);
+            this.outputFilePath = outputFilePath;
+            watcher.onDidCreate(() => {
+                vscode.window.showInformationMessage("Created!");
+                this.doUpdateResponse();
+                //this.openViewOnSide(outputFilePath);
+                //this._update();
+            });
+            watcher.onDidChange(() => {
+                vscode.window.showInformationMessage("Changed!");
+                this.doUpdateResponse();
+                //this.openViewOnSide(outputFilePath);
+                //this._update();
+            });
+        }
+    }
+    static createOrShow(extensionUri) {
+        const column = vscode.window.activeTextEditor
+            ? vscode.window.activeTextEditor.viewColumn
+            : undefined;
+        // If we already have a panel, show it.
+        if (CatCodingPanel.currentPanel) {
+            CatCodingPanel.currentPanel._panel.reveal(column);
+            return;
+        }
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(CatCodingPanel.viewType, 'Cat Coding', column || vscode.ViewColumn.One, getWebviewOptions(extensionUri));
+        CatCodingPanel.currentPanel = new CatCodingPanel(panel, extensionUri);
+    }
+    static revive(panel, extensionUri) {
+        CatCodingPanel.currentPanel = new CatCodingPanel(panel, extensionUri);
+    }
+    async doUpdateResponse() {
+        if ( /*this.referlOutputDiscovered &&*/this.outputFilePath != undefined) {
+            const uri = this.outputFilePath;
+            const JsonString = await vscode.workspace.openTextDocument(uri).then((document) => {
+                return document.getText();
+            });
+            const response = JSON.parse(JsonString);
+            this._panel.webview.postMessage({ command: 'updateResponse', data: response });
+        }
+    }
+    doRefactor() {
+        // Send a message to the webview webview.
+        // You can send any JSON serializable data.
+        this._panel.webview.postMessage({ command: 'refactor' });
+    }
+    dispose() {
+        CatCodingPanel.currentPanel = undefined;
+        // Clean up our resources
+        this._panel.dispose();
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+    async _update() {
+        const webview = this._panel.webview;
+        this._panel.title = "TEST";
+        const promise = this._getHtmlForWebview(webview);
+        this._panel.webview.html = await Promise.resolve(promise);
+    }
+    openViewOnSide(uri) {
+        const visible = vscode.window.visibleTextEditors;
+        const docs = [];
+        for (const visi of visible) {
+            docs.push(visi.document.uri.toString());
+        }
+        console.log(docs);
+        //vscode.window.showTextDocument(uri, { viewColumn: vscode.ViewColumn.Beside });
+    }
+    async _getHtmlForWebview(webview) {
+        // Local path to main script run in the webview
+        const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js');
+        // And the uri we use to load this script in the webview
+        const scriptUri = (scriptPathOnDisk).with({ 'scheme': 'vscode-resource' });
+        // Local path to css styles
+        const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+        const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
+        // Uri to load styles into webview
+        const stylesResetUri = webview.asWebviewUri(styleResetPath);
+        const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+        // Use a nonce to only allow specific scripts to be run
+        const nonce = getNonce();
+        //TODO: if the response is undefined
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<h1>Hello ELTE!</h1>
+				<h1 id="lines-of-code-counter">0</h1>
+				<h3>Response: <span id="response"></span></h3>
+				<p id="refac">~~PLACHOLDER~~</p>
+				<p id="ws">NO INFO</p>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+    }
+}
+CatCodingPanel.viewType = 'catCoding';
+function getNonce() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+//# sourceMappingURL=extension.js.map
